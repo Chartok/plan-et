839d6d1cfa4fa00f67540c91a4b18591bb81de03
Chartok <m.bhimjee@outlook.com>
2023-06-07 17:35:16 -0500

    finished js

--- Develop/script.js
+++ Develop/script.js
@@ -2,24 +2,42 @@ $(document).ready(function () {
     // Initialize current day
     let day = dayjs();
 
+    // Logic to display current day
+    $('#currentDay').text(day.format('dddd, MMMM D, YYYY'));
+
+    // Loop to generate time slots from 8am - 5pm
     for (let i = 8; i <= 17; i++) {
         let timeSlot = day.hour(i).minute(0).second(0);
         let event = localStorage.getItem(timeSlot.format('HH:mm')) || '';
 
+
+        // Loop to set past/present/future status conditional
+        let status = '';
+        if (dayjs().hour() > i) {
+            status = 'past';
+        } else if (dayjs().hour() == i) {
+            status = 'present';
+        } else {
+            status = 'future';
+        }
+
+        // jQuery to generate and append time slots
         $('#calendar').append(`
-            <div class="hourSlot">
-                <span class="hour">${timeSlot.format('hh:mm A')}</span>
-                <input type="text" class="event" id="${timeSlot.format('HH:mm')}" value="${event}" />
-                <button class="saveEvent">Save</button>
+            <div class="hourSlot ${status}">
+            <span class="hour">${timeSlot.format('hh:mm A')}</span>
+            <input type="text" class="event" id="${timeSlot.format('HH:mm')}" value="${event}" />
+            <button class="saveEvent">Save</button>
             </div>
-        `)
+            `)
     }
 
+    // Event listener for save events
     $('.saveEvent').click(function() {
         let eventTime = $(this).siblings('input').attr('id');
         let eventDesc = $(this).siblings('input').val();
 
 
+        // Store event to local storage
         localStorage.setItem(eventTime, eventDesc);
     });
 
