cf4b4e180eb64c61b11cf8e11655224db69f3cac
Chartok <m.bhimjee@outlook.com>
2023-06-07 21:53:52 -0500

    working; mvp

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,79 @@
-$(document).ready(function () {
-    // Initialize current day
-    let day = dayjs();
+// Using async/await & try/catch for error handling
+(async () => {
+    try {
+        // Prompt user to allow notifications for saved events
+        const permission = await Notification.requestPermission();
 
-    // Logic to display current day
-    $('#currentDay').text(day.format('dddd, MMMM D, YYYY'));
+        // Initialize current day; user template string to display date
+        const day = dayjs();
+        $('#currentDay').text(`${day.format('dddd, MMMM D, YYYY')}`);
 
+        await Promise.all([...Array(10).keys()].map(async (i) => {
+            i += 8; // Offset hour to start index at 8AM
 
-    // Loop to generate time slots from 8am - 5pm
-    for (let i = 8; i <= 17; i++) {
-        let timeSlot = day.hour(i).minute(0).second(0);
-        let event = localStorage.getItem(timeSlot.format('HH:mm')) || '';
-        let status = '';
+            const timeSlot = day.hour(i).minute(0).second(0);
+            let event = localStorage.getItem(timeSlot.format('HH:mm')) || '';
+            let status = '';
 
-        // Loop to set past/present/future status conditional
-        if (dayjs().hour() > i) {
-            status = 'past';
-        } else if (dayjs().hour() == i) {
-            status = 'present';
-        } else {
-            status = 'future';
-        }
+            if (dayjs().hour() > i) {
+                status = 'past';
+            } else if (dayjs().hour() === i) {
+                status = 'present';
+            } else {
+                status = 'future';
+            }
 
-        // jQuery to generate and append time slots
-        $('#calendar').append(`
-            <div class="hourSlot ${status}">
-            <span class="hour">${timeSlot.format('hh:mm A')}</span>
-            <input type="text" class="event" id="${timeSlot.format('HH:mm')}" value="${event}" />
-            <button class="saveEvent">Save</button>
-            </div>
-            `)
+            await $('#calendar').append(`
+                <div class="row p-b-4 hourSlot ${status}">
+                <div class="col-8"><input type="text" class="lead form-control event" id="${timeSlot.format('HH:mm')}" value="${event}" /></div>
+                <div class="col hour">${timeSlot.format('hh:mm A')}</div>
+                <div class="col start-10%"><button class="btn btn-primary saveEvent">Save</button></div>
+                <div class="row border-bottom saved-events">
+                <div class="col m-b-3">
+                <p><strong>Saved Event:</strong> ${event}</p>
+                <div class="col mb-5">
+                <button class="btn btn-danger btn-sm clearEvent">Clear</button>
+                </div>
+                </div>
+                </div>
+                </div>
+                `);
+
+            //
+                if (event && permission === 'granted' && status !== 'past') {
+                    const eventTimeinMs = timeSlot.diff(dayjs(), 'millisecond');
+                    setTimeout(() => new Notification('Calendar Event', { body: event }), eventTimeInMs);
+                }
+
+        }));
+
+        // Logic for clear all button
+        $('#clearAll').click(function() {
+            localStorage.clear();
+            $('.event').val('');
+            $('.saved-events p').html('<strong>Saved Event:</strong> ');
+        });
+
+        // Logic for clear event
+        $('#calendar').on('click', '.clearEvent', function() {
+            const eventTime = $(this).siblings('input').attr('id');
+            localStorage.removeItem(eventTime);
+            $(this).siblings('input').val('');
+            $(this).parent().parent().next('.saved-events').find('p').html('<strong>Saved Event:</strong> ');
+        });
+
+        $('#calendar').on('click', '.saveEvent', async function () {
+            const eventTime = $(this).sibling('input').attr('id');
+            const eventDesc = $(this).sibling('input').val();
+
+            localStorage.setItem(eventTime, eventDesc);
+
+            await $(this).parent().parent().next('.saved-events').find('p').html(`<strong>Saved Event:</strong> ${eventDesc}`);
+
+            $('#saveModal').modal('show');
+        });
+    } catch (err) {
+        console.error(err);
     }
 
-    // Event listener for save events
-    $('.saveEvent').click(function() {
-        let eventTime = $(this).siblings('input').attr('id');
-        let eventDesc = $(this).siblings('input').val();
-
-
-        // Store event to local storage
-        localStorage.setItem(eventTime, eventDesc);
-
-        $('#modalMessage').text('Event saved for ${eventTime}')
-        $('#myModal').modal('show');
-
-
-        $(this).siblings('.savedEvent').text(eventDesc);
-    });
-});
+})();
