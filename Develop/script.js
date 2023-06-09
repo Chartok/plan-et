29acdb832d0c07a5d540d3cfbef45020358e4478
Chartok <m.bhimjee@outlook.com>
2023-06-08 22:23:41 -0500

    FIXED

--- Develop/script.js
+++ Develop/script.js
@@ -24,58 +24,58 @@
             }
 
             await $('#calendar').append(`
-                <div class="row p-b-4 hourSlot ${status}">
+            <div class="row p-b-4 hourSlot ${status}">
                 <div class="col-8 mt-4"><input type="text" class="form-control event" id="${timeSlot.format('HH:mm')}" value="${event}" /></div>
                 <div class="col hour">${timeSlot.format('hh:mm A')}</div>
                 <div class="col start-10%"><button class="btn btn-primary saveEvent">Save</button></div>
-                <div class="row border-bottom saved-events">
+            </div>
+            <div class="row border-bottom saved-events">
                 <div class="col mb-3">
-                <p><strong>Saved Event:</strong> ${event}</p>
+                    <p><strong>Saved Event:</strong> ${event}</p>
+                </div>
                 <div class="col mb-3">
-                <button class="btn btn-danger btn-sm clearEvent">Clear</button>
+                    <button class="btn btn-danger btn-sm clearEvent">Clear</button>
                 </div>
-                </div>
-                </div>
-                </div>
-                `);
+            </div>
+        `);
+
 
             //
-                if (event && permission === 'granted' && status !== 'past') {
-                    const eventTimeInMs = timeSlot.diff(dayjs(), 'millisecond');
-                    setTimeout(() => new Notification('Calendar Event', { body: event }), eventTimeInMs);
-                }
+            if (event && permission === 'granted' && status !== 'past') {
+                const eventTimeInMs = timeSlot.diff(dayjs(), 'millisecond');
+                setTimeout(() => new Notification('Calendar Event', { body: event }), eventTimeInMs);
+            }
 
         }));
 
         // Logic for clear all events from local storage
-        $('#clearAll').click(function() {
+        $('#clearAll').click(function () {
             localStorage.clear();
             $('.event').val('');
             $('.saved-events p').html('<strong>Saved Event:</strong> ');
         });
 
         // Logic for clear event from local storage
-        $('#calendar').on('click', '.clearEvent', function() {
-            const eventTime = $(this).siblings('input').attr('id');
-            localStorage.removeItem(eventTime);
-            $(this).siblings('input').val('');
-            $(this).parent().parent().next('.saved-events').find('p').html('<strong>Saved Event:</strong> ');
-        });
-
-        // Logic to save events to local storage
         $('#calendar').on('click', '.saveEvent', async function () {
-            const eventTime = $(this).siblings('input').attr('id');
-            const eventDesc = $(this).siblings('input').val();
+            const eventTime = $(this).parent().parent().find('input').attr('id');
+            const eventDesc = $(this).parent().parent().find('input').val();
 
             localStorage.setItem(eventTime, eventDesc);
-
 
             await $(this).parent().parent().next('.saved-events').find('p').html(`<strong>Saved Event:</strong> ${eventDesc}`);
 
             $('#saveModal').modal('show');
         });
+
+        $('#calendar').on('click', '.clearEvent', function () {
+            const eventTime = $(this).parent().parent().prev().find('input').attr('id');
+            localStorage.removeItem(eventTime);
+            $(this).parent().parent().prev().find('input').val('');
+            $(this).parent().siblings().find('p').html('<strong>Saved Event:</strong> ');
+        });
+
+
     } catch (err) {
         console.error(err);
     }
-
 })();
